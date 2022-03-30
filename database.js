20efc6697da00379b38ba2169353b728f86f3319
elmersito <101758896+elmersito@users.noreply.github.com>
2022-03-29 19:15:05 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-    host: "localhost",
-    user: "root",
-    password: "",
+    host: "us-cdbr-east-05.cleardb.net",
+    user: "bee50dcc88dce8",
+    password: "279ef05e",
     port: 3306,
-    database: "adoption_system",
-    secret_key: "kj987zhf5643qsf5fd3q"
+    database: "heroku_387d6fe6b649c65",
+    secret_key: "dghssdfh"
 };
 exports.config = config;
